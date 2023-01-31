694ddfc6cba8967033152eb7db91912d7332b204
Lore-jls <95663514+lorejls@users.noreply.github.com>
2023-01-31 13:41:24 -0300

    respaldo La Liga

---
+++ Js/match-table.js
@@ -0,0 +1,58 @@
+// console.log(matchesData)
+
+// jornada 1.1
+// let matchDayGame = matchesData.matches[0].matchday
+// console.log(matchDayGame)
+
+// let homeTeamName = matchesData.matches[0].homeTeam.name
+// console.log(homeTeamName)
+
+// let awayTeamName = matchesData.matches[0].awayTeam.name
+// console.log(awayTeamName)
+
+// let finalScore = matchesData.matches[0].homeTeam.name + ' ' + matchesData.matches[0].score.fullTime.homeTeam + ' - ' + matchesData.matches[0].score.fullTime.awayTeam + ' '+ matchesData.matches[0].awayTeam.name
+// console.log(finalScore)
+
+// let gameDate = matchesData.matches[0].utcDate
+// console.log(gameDate)
+
+// let homeTeamCrest = "https://crests.football-data.org/" + matchesData.matches[0].homeTeam.id + ".svg"
+// console.log(homeTeamCrest)
+
+// //jornada 1.2
+// let matchDayGameTwo = matchesData.matches[1].matchday
+// console.log(matchDayGameTwo)
+
+// let homeTeamNameTwo = matchesData.matches[1].homeTeam.name
+// console.log(homeTeamNameTwo)
+
+// let awayTeamNameTwo = matchesData.matches[1].awayTeam.name
+// console.log(awayTeamNameTwo)
+
+// let finalScoreTwo = matchesData.matches[1].homeTeam.name + ' ' + matchesData.matches[1].score.fullTime.homeTeam + ' - ' + matchesData.matches[1].score.fullTime.awayTeam + ' '+ matchesData.matches[1].awayTeam.name
+// console.log(finalScoreTwo)
+
+// let gameDateTwo = matchesData.matches[1].utcDate
+// console.log(gameDateTwo)
+
+// let homeTeamCrestTwo = "https://crests.football-data.org/" + matchesData.matches[1].homeTeam.id + ".svg"
+// console.log(homeTeamCrestTwo)
+
+// //jornada 1.3
+// let matchDayGameThree = matchesData.matches[2].matchday
+// console.log(matchDayGameThree)
+
+// let homeTeamNameThree = matchesData.matches[2].homeTeam.name
+// console.log(homeTeamNameThree)
+
+// let awayTeamNameThree = matchesData.matches[2].awayTeam.name
+// console.log(awayTeamNameThree)
+
+// let finalScoreThree = matchesData.matches[2].homeTeam.name + ' ' + matchesData.matches[2].score.fullTime.homeTeam + ' - ' + matchesData.matches[2].score.fullTime.awayTeam + ' '+ matchesData.matches[2].awayTeam.name
+// console.log(finalScoreThree)
+
+// let gameDateThree = matchesData.matches[2].utcDate
+// console.log(gameDateThree)
+
+// let homeTeamCrestThree = "https://crests.football-data.org/" + matchesData.matches[2].homeTeam.id + ".svg"
+// console.log(homeTeamCrestThree)
---
+++ Js/stand-table.js
@@ -0,0 +1,83 @@
+// let matchesPlayed = standingsData.standings[0].table[0].playedGames
+// console.log(matchesPlayed)
+
+// let wonTotal = standingsData.standings[0].table[0].won
+// console.log(wonTotal)
+
+// let drawTotal = standingsData.standings[0].table[0].draw
+// console.log(drawTotal)
+
+// let lostTotal = standingsData.standings[0].table[0].lost
+// console.log(lostTotal)
+
+// let goalsForTotal = standingsData.standings[0].table[0].goalsFor
+// console.log(goalsForTotal)
+
+// let goalsAgainstTotal = standingsData.standings[0].table[0].goalsAgainst
+// console.log(goalsAgainstTotal)
+
+// let goalDifferenceTotal = standingsData.standings[0].table[0].goalDifference
+// console.log(goalDifferenceTotal)
+
+// let pointsTotal = standingsData.standings[0].table[0].points
+// console.log(pointsTotal)
+
+let standingsTable = standingsData.standings[0].table
+// console.log(standingsTable)
+
+function tableBuilder(results){
+    let table = document.getElementById("table-body")
+    for(let i=0; i<results.length; i++){
+        let tr = document.createElement("tr")
+
+        let position = document.createElement("p")
+        position.innerHTML = results[i].position
+
+        let team = document.createElement("p")
+        team.innerHTML = results[i].team.name
+
+        let matchesplayed = document.createElement("p")
+        matchesplayed.innerHTML = results[i].playedGames
+        console.log(matchesplayed)
+
+        let won = document.createElement("p")
+        won.innerHTML = results[i].won
+        console.log(won)
+
+        let draw = document.createElement("p")
+        draw.innerHTML = results[i].won
+        console.log(draw)
+
+        let losts = document.createElement("p")
+        losts.innerHTML = results[i].won
+        console.log(losts)
+
+        let goalsFor = document.createElement("p")
+        goalsFor.innerHTML = results[i].won
+        console.log(goalsFor)
+        
+        let goalsAgainst = document.createElement("p")
+        goalsAgainst.innerHTML = results[i].won
+        console.log(goalsAgainst)
+
+        let goalDifference = document.createElement("p")
+        goalDifference.innerHTML = results[i].won
+        console.log(goalDifference)
+
+        let points = document.createElement("p")
+        points.innerHTML = results[i].won
+        console.log(points)
+
+        let finalStandingTable = [position,team,matchesplayed,won, draw, losts, goalsFor, goalsAgainst, goalDifference, goalDifference, points]
+            for(let j=0; j<finalStandingTable.length; j++){
+                const td= document.createElement("td")
+
+                td.append(finalStandingTable[j])
+                tr.append(td)
+            }
+        table.append(tr)
+    }
+}
+
+tableBuilder(standingsTable)
+
